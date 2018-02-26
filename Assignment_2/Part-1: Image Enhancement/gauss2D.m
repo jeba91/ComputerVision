e982fb0b4797d302994b8f8620030a5d00309262
bobvandenhoogen <bobvandenhoogen@hotmail.com>
2018-02-26 11:30:28 +0100

    kleine veranderingen

--- Assignment_2/Part-1: Image Enhancement/gauss2D.m
+++ Assignment_2/Part-1: Image Enhancement/gauss2D.m
@@ -1,6 +1,5 @@
 function G = gauss2D( sigma , kernel_size )
     %% solution
     x = gauss1D(sigma, kernel_size);
-    y = (gauss1D(sigma, kernel_size))';
-    G = conv2(x, y);
+    G = conv2(x, x');
 end
