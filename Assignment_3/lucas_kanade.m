c215b45b82322a30087e98ddf190abb43e46613b
Jeroen Baars <jeroen.j.baars@gmail.com>
2018-03-07 15:38:35 +0100

    function redefined

--- Assignment_3/lucas_kanade.m
+++ Assignment_3/lucas_kanade.m
@@ -1,4 +1,3 @@
-
 sphere1 = imread('sphere1.ppm');
 sphere2 = imread('sphere2.ppm');
 
@@ -8,65 +7,92 @@ sphere2gr = rgb2gray(sphere2);
 synth1 = imread('synth1.pgm');
 synth2 = imread('synth2.pgm');
 
-imshow(synth1)
-
 region = 15;
 
-V1  = LK(sphere1gr,sphere2gr,region);
-visualize(sphere1, V1, region);
+%V1  = LK(sphere1gr,sphere2gr,region);
+%visualize(sphere1, V1, region);
 
-V2  = LK(synth1,synth2,region);
-visualize(synth1, V2, region);
+%V2  = LK(synth1,synth2,region);
+%visualize(synth1, V2, region);
 
-function [V] = LK(im1, im2, regionsize)
+%V3 = LK(sphere1gr,sphere2gr,region,[15 30 45 60 75],[75 75 75 75 75])
+
+function [V] = LK(im1, im2, regionsize, list1, list2)
+    if mod(regionsize,2) == 0
+        "no even numbers for regionsize"
+        return
+    end
+    
     im1 = (im2double(im1));
     im2 = (im2double(im2));
     
-    if mod(size(im1,1),regionsize) ~= 0
-        'hal'
-        end_m = (size(im1,1) - mod(size(im1,1),regionsize)) - regionsize+1;
-        end_n = (size(im1,2) - mod(size(im1,2),regionsize)) - regionsize+1;
+    if exist('list1') == 1
+        V = zeros(length(list1), 2)
+        for m = 1:length(list1)
+            V(m,:) = LucasKanade(im1,im2,regionsize,[list1(m),list2(m)]);
+        end
+        [x,y] = meshgrid(list1,list2);
+        quiver(x,y,V(1,:),V(2,:));
     else
-        end_m = size(im1,1)-(regionsize+2)
-        end_n = size(im1,2)-(regionsize+2)
-    end
+        if mod(size(im1,1),regionsize) ~= 0
+            end_m = (size(im1,1) - mod(size(im1,1),regionsize)) - regionsize+1;
+            end_n = (size(im1,2) - mod(size(im1,2),regionsize)) - regionsize+1;
+        else
+            end_m = size(im1,1)-(regionsize+2)
+            end_n = size(im1,2)-(regionsize+2)
+        end
 
-    [Gx, Gy] = gradient(fspecial('gaussian', 15, 1));
-    Ix = conv2(im1, Gx, 'same');
-    Iy = conv2(im1, Gy, 'same');
-    It = im2 - im1;
-    
-    gridsize = (size(im1,1) - mod(size(im1,1),regionsize)) / regionsize;
-    
-    V=zeros(gridsize,gridsize,2);
-    A=zeros((regionsize^2),2);
-    b=zeros((regionsize^2),1);
+        gridsize = (size(im1,1) - mod(size(im1,1),regionsize)) / regionsize;
+        V=zeros(gridsize,gridsize,2);
 
-    for m = 1:regionsize:end_m
-        for n = 1:regionsize:end_n
-            counter = 1;
-            for i = m:(m+regionsize)
-                for j = n:(n+regionsize)
-                    A(counter,1) = Ix(i,j);
-                    A(counter,2) = Iy(i,j); 
-                    b(counter,1) = -It(i,j);
-                    counter = counter + 1;
-                end
+        for m = 1:regionsize:end_m
+            for n = 1:regionsize:end_n
+
+                half = ((regionsize - mod(regionsize,2)) / 2);
+
+                v = LucasKanade(im1,im2,regionsize,[m+half,n+half]);
+
+                x = uint8((m-1)/regionsize)+1;            
+                y = uint8((n-1)/regionsize)+1;
+
+                V(x,y,1) = v(1);
+                V(x,y,2) = v(2);           
             end
-            C = (inv(transpose(A)*A))*(transpose(A)*b);
-            x = uint8((m-1)/regionsize)+1;
-            y = uint8((n-1)/regionsize)+1;
-            V(x,y,1) = C(1,:);
-            V(x,y,2) = C(2,:);
         end
     end
 end
 
-function [] = visualize(im, V, regionsize)
+function [v] = LucasKanade(image1, image2, regionsize, centerpoint)
+
+    [Gx, Gy] = gradient(fspecial('gaussian', 15, 1.5));
+    Ix = conv2(image1, Gx, 'same');
+    Iy = conv2(image2, Gy, 'same');
+    It = image2 - image1;
     
+    start_i = centerpoint(1) - ((regionsize - mod(regionsize,2)) / 2);
+    start_j = centerpoint(2) - ((regionsize - mod(regionsize,2)) / 2);
+    
+    A=zeros((regionsize^2),2);
+    b=zeros((regionsize^2),1);
+    
+    counter = 1;
+    
+    for i = start_i:start_i+regionsize
+        for j = start_j:start_j+regionsize
+            A(counter,1) = Ix(i,j);
+            A(counter,2) = Iy(i,j); 
+            b(counter,1) = -It(i,j);
+            counter = counter + 1;
+        end
+    end
+    
+    v = (inv(transpose(A)*A))*(transpose(A)*b);   
+end
+
+function [] = visualize(im, V, regionsize)
+
     middle = 1+ ((regionsize - mod(regionsize,2)) / 2);
 
-    
     xcoordinates = middle:regionsize:size(im,1)-1; 
     ycoordinates = middle:regionsize:size(im,1)-1;
     
