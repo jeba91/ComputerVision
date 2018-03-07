377599f540cc38c9deec6e888443f7d3d828264b
Jeroen Baars <jeroen.j.baars@gmail.com>
2018-03-07 17:47:18 +0100

    commenting

--- Assignment_3/LK.m
+++ Assignment_3/LK.m
@@ -1,18 +1,28 @@
 function [V] = LK(im1, im2, regionsize, list1, list2)
+    %With 3 parameter input a matrix V is returned with the section values
+    %With 5 parameter input a list V is returned with values per
+    %coordinate, also this list is plotted on the current figure
+
+    %Only even region size for LK algorithms (So there is a middle)
     if mod(regionsize,2) == 0
         "no even numbers for regionsize"
         return
     end
     
+    %Convert images to doubles
     im1 = (im2double(im1));
     im2 = (im2double(im2));
     
-    if exist('list1') == 1
+    %If there are 2 lists given, plot these on the image
+    if exist('list1') == 1 && exist('list2') == 1
         V = zeros(2, length(list1));
         for m = 1:length(list1)
+            %Get Vy and Vx for every given coordinate
             V(:,m) = LucasKanade(im1,im2,regionsize,[list1(m),list2(m)]);
         end
+        %Plot Vy and Vx
         quiver(list2,list1,V(1,:),V(2,:));
+    %Else create V matrix with every value for every non overlapping region
     else
         if mod(size(im1,1),regionsize) ~= 0
             end_m = (size(im1,1) - mod(size(im1,1),regionsize)) - regionsize+1;
@@ -24,7 +34,8 @@ function [V] = LK(im1, im2, regionsize, list1, list2)
 
         gridsize = (size(im1,1) - mod(size(im1,1),regionsize)) / regionsize;
         V=zeros(gridsize,gridsize,2);
-
+           
+        %Get every Vx Vy for the V matrix
         for m = 1:regionsize:end_m
             for n = 1:regionsize:end_n
 
@@ -43,7 +54,7 @@ function [V] = LK(im1, im2, regionsize, list1, list2)
 end
 
 function [v] = LucasKanade(image1, image2, regionsize, centerpoint)
-
+    %Calculate the LK value for a specific centerpoint
     [Gx, Gy] = gradient(fspecial('gaussian', 15, 1.5));
     Ix = conv2(image1, Gx, 'same');
     Iy = conv2(image2, Gy, 'same');
@@ -57,6 +68,7 @@ function [v] = LucasKanade(image1, image2, regionsize, centerpoint)
     
     counter = 1;
     
+    %Loop through all the pixels of the region and fill the A and b
     for i = start_i:start_i+regionsize
         for j = start_j:start_j+regionsize
             A(counter,1) = Ix(i,j);
@@ -66,5 +78,6 @@ function [v] = LucasKanade(image1, image2, regionsize, centerpoint)
         end
     end
     
+    %Calculate the v value from the A and b
     v = (inv(transpose(A)*A))*(transpose(A)*b);   
 end
--- Assignment_3/lucas_kanade.m
+++ Assignment_3/lucas_kanade.m
@@ -1,3 +1,4 @@
+%Read in images, convert sphere images to grayscale voor LK algorithm
 sphere1 = imread('sphere1.ppm');
 sphere2 = imread('sphere2.ppm');
 
@@ -7,16 +8,18 @@ sphere2gr = rgb2gray(sphere2);
 synth1 = imread('synth1.pgm');
 synth2 = imread('synth2.pgm');
 
-region = 15;
+%Region size paramter
+region = 5;
 
-%V1  = LK(sphere1gr,sphere2gr,region);
-%visualize(sphere1, V1, region);
+%Use LK algorithm on sphere image
+V1  = LK(sphere1gr,sphere2gr,region);
+visualize(sphere1, V1, region);
 
-%V2  = LK(synth1,synth2,region);
-%visualize(synth1, V2, region);
+%Use LK algoritm on synth image
+V2  = LK(synth1,synth2,region);
+visualize(synth1, V2, region);
 
-%V3 = LK(sphere1gr,sphere2gr,region,[15 30 45 60 75],[75 75 75 75 75])
-
+%Function to visualize sections arrow on images
 function [] = visualize(im, V, regionsize)
 
     middle = 1+ ((regionsize - mod(regionsize,2)) / 2);
@@ -30,4 +33,6 @@ function [] = visualize(im, V, regionsize)
     RI.XWorldLimits = [0 size(im,1)]; RI.YWorldLimits = [0 size(im,2)];
     figure; imshow(im,RI); hold on; quiver(x,y,V(:,:,1),V(:,:,2));
     
+    
+    saveas(gcf,'sphere5.png')
 end
