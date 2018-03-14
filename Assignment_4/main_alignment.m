2fdb0a911c6fc808567ecaf64be79c5cd6206a0f
bobvandenhoogen <bobvandenhoogen@hotmail.com>
2018-03-14 13:57:33 +0100

    minor update

--- Assignment_4/main_alignment.m
+++ Assignment_4/main_alignment.m
@@ -2,16 +2,15 @@ boat1 = single(imread('boat1.pgm'));
 boat2 = single(imread('boat2.pgm'));
 
 figure
-imshowpair(imread('boat1.pgm'),imread('boat2.pgm'))
+imshowpair(imread('boat1.pgm'),imread('boat2.pgm'), "montage")
 hold on;
+% 
+% [f1, d1] = vl_sift(boat1);
+% [f2, d2] = vl_sift(boat2);
 
-[f1, d1] = vl_sift(boat1);
-[f2, d2] = vl_sift(boat2);
+[f1, f2, matches] = keypoint_matching(boat1, boat2);
 
-matches = keypoint_matching(boat1, boat2);
-
-perm = randperm(size(matches,2));
-sel = perm(1:50);
+sel = randperm(size(matches,2), 50);
 
 pointim1 = matches(1,sel);
 pointim2 = matches(2,sel);
@@ -19,10 +18,10 @@ pointim2 = matches(2,sel);
 points1 = f1(:,pointim1);
 points2 = f2(:,pointim2);
 
-points2(1,:) = points2(1,:);
+points2(1,:) = points2(1,:) + 850;
 
-x = zeros(2,50)
-y = zeros(2,50)
+x = zeros(2,50);
+y = zeros(2,50);
 
 x(1,:) = points1(1,:);
 x(2,:) = points2(1,:);
