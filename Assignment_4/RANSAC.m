d1eba930704da210bfadad9421c247ed5c4f4817
bobvandenhoogen <bobvandenhoogen@hotmail.com>
2018-03-14 14:00:53 +0100

    bugfix

--- Assignment_4/RANSAC.m
+++ Assignment_4/RANSAC.m
@@ -5,8 +5,8 @@ function [m, t] = RANSAC(f1, f2, sel)
     x2 = f2(1, sel(2, :));
     y2 = f2(2, sel(2, :));
     %Find transformation parameters by solving x = (A^T A)^?1 A^T b
-    A = [x1', y1', zeros(2,size(X1,2))', ones(1,size(X1,2))', zeros(1,size(X1,2))';...
-         zeros(2,size(X1,2)), x1', y1', zeros(1,size(X1,2))', ones(1,size(X1,2))];
+    A = [x1', y1', zeros(2,size(x1,2))', ones(1,size(x1,2))', zeros(1,size(x1,2))';...
+         zeros(2,size(x1,2)), x1', y1', zeros(1,size(x1,2))', ones(1,size(x1,2))];
     b = [x2'; y2'];
     tpoints = pinv(A) * b;
     % transformationpoints looks like [m1,m2,m3,m4,t1,t2], return m and t
